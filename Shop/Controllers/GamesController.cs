d75807595d893bb01b09d687f91fd6cf42fa7f26
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-18 18:49:08 +0300

    Add Controller, GameController

---
+++ Shop/Controllers/GamesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        public GamesService _gamesService;
+
+        public GamesController(GamesService gamesService)
+        {
+            _gamesService = gamesService;
+        }
+
+        [HttpGet("get-games")]
+        public IActionResult GetGames()
+        {
+            var gamse = _gamesService.GetGames();
+            return Ok(gamse);
+        }
+    }
+}
