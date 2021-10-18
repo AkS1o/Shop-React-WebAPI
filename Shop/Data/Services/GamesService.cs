165316794bf845b72da0bd24df5fb0724585c8b5
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-18 18:50:24 +0300

    Add Services

---
+++ Shop/Data/Services/GamesService.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Services
+{
+    public class GamesService
+    {
+        private readonly AppDbContext _context;
+        public GamesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Game> GetGames()
+        {
+            var games = _context.Games;
+            return games;
+        }
+    }
+}
