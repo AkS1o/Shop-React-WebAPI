d37480b7b7af196530b5056aaeb9bdc88971fa04
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-18 18:54:10 +0300

    Init DateBase

---
+++ Shop/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shop.Data.Models;
+
+namespace Shop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
