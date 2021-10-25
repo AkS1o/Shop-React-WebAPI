c1b31a8b88f04bce34ebf2727a5213d92f73020c
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-25 18:59:25 +0300

    add mainplatform and addplatform

---
+++ Shop/client-app/src/views/Platform/AddPlatform.js
@@ -0,0 +1,97 @@
+﻿import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Component } from "react";
+import { Redirect } from "react-router-dom";
+import APIService from "../../services/APIService";
+import { v4 as uuidv4 } from 'uuid';
+
+import { addPlatform } from "../../actions/PlatformListAction";
+
+import React from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+class AddPlatform extends Component {
+    state = {
+        editorState: EditorState.createEmpty(),
+        Name: "",
+        isRedirect: false
+    }
+
+    onGetName = (e) => {
+        const name = e.target.value;
+        this.setState({
+            Name: name
+        })
+    }
+
+    CreatePlatform = (e) => {
+        e.preventDefault();
+        const { Name } = this.state;
+        const newPlatform = {
+            Name
+        }
+        let tmplist = this.props.PlatformList.slice();
+        tmplist.push(newPlatform);
+        this.props.addPlatform(tmplist);
+        APIService.addPlatform(newPlatform);
+
+        this.setState({
+            isRedirect: true
+        })
+    }
+
+    onEditorStateChange = (editorState) => {
+        this.setState({
+            editorState,
+        });
+    };
+
+    render() {
+        let { isRedirect } = this.state;
+        if (isRedirect === true) {
+            return <Redirect to="/admin" />
+        }
+        return (
+            <Fragment>
+                <form method="post" enctype="multipart/form-data" onSubmit={this.CreatePlatform}>
+                    <div className="row">
+                        <div className="col-8">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h3>Product Information</h3>
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <div class="form-group">
+                                                <label>Name</label>
+                                                <input type="text" onChange={this.onGetName} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <Link to="/admin/products" class="btn btn-outline-white mx-2">Back</Link>
+                        <input type="submit" class="btn btn-primary mx-2" value="Create" />
+                    </div>
+                </form>
+            </Fragment >
+        )
+    }
+}
+
+const mapStateToProps = ({ PlatformListReducer }) => {
+    const { PlatformList } = PlatformListReducer;
+    return { PlatformList }
+}
+
+const mapDispatchToProps = {
+    addPlatform
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPlatform);
---
+++ Shop/client-app/src/views/Platform/MainPlatform.js
@@ -0,0 +1,62 @@
+﻿import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux"
+import { useEffect } from "react";
+
+import { getAllPlatforms } from "../../actions/PlatformListAction"
+
+import apiService from "../../services/APIService";
+
+import Pagination from "../../../src/components/Pagination/Pagination"
+
+import TablePlatform from "../../components/Table/TablePlatforms/TablePlanforms";
+
+
+const MainPlatform = ({ PlatformList, getAllPlatforms }) => {
+
+    useEffect(() => {
+        apiService.fetchPlatformList().then(data => {
+            getAllPlatforms(data.List);
+        });
+    }, []);
+
+    const item = PlatformList.map(listItem => {
+        return (
+            <TablePlatform key={listItem.Id}
+                {...listItem} />
+        )
+    });
+    return (
+        <Fragment>
+            <Link to="/admin/add-platform" className="btn btn-white">Add Platform</Link>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox" />
+                        </th>
+                        <th>Platform</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {item.length > 0 ? item : <h4>Platform list is empty.</h4>}
+                </tbody>
+            </table>
+            <div className="d-flex align-items-center justify-content-between">
+                <div>Showing platforms 1 to 5 of 12</div>
+                <Pagination />
+            </div>
+        </Fragment >
+    )
+}
+
+const mapStateToProps = ({ PlatformListReducer }) => {
+    const { PlatformList } = PlatformListReducer;
+    return { PlatformList }
+}
+
+const mapDispatchToProps = {
+    getAllPlatforms
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPlatform);
