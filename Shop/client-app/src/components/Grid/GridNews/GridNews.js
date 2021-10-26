8a468da657b53d06faadad9e3c03114f335e1f99
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-26 15:36:53 +0300

    Add GridNews

--- Shop/client-app/src/components/Grid/GridNews/GridNews.js
+++ Shop/client-app/src/components/Grid/GridNews/GridNews.js
@@ -1,24 +1,45 @@
 import { Fragment } from "react";
-
+import { useEffect } from "react";
+import { connect } from "react-redux"
 // Import components
 import CardNews from "../../Card/CardNews/CardNews";
 import Pagination from "../../Pagination/Pagination";
 
-const NewsGrid = () => {
+import { getAllNews } from "../../../actions/NewsListAction"
+
+// Import Services
+import apiService from "../../../services/APIService";
+
+const NewsGrid = ({ NewsList, getAllNews }) => {
+    useEffect(() => {
+        apiService.fetchNewsList().then(data => {
+            getAllNews(data.List);
+        })
+    }, []);
+
+    const item = NewsList.map(listItem => {
+        return (
+            <CardNews key={listItem.Id}
+                {...listItem} />
+        )
+    });
+
     return (
-        <Fragment>
-             <section className="news">
-                <div className="row">
-                    <CardNews />
-                    <CardNews />
-                    <CardNews />
-                    <CardNews />
-                    <CardNews />
-                </div>
-                <Pagination />
-            </section>
-        </Fragment>
+        <section className="news">
+            <div className="row">
+                {item.length > 0 ? item : <h4>Contact list is empty.</h4>}
+            </div>
+        </section>
     )
 }
 
-export default NewsGrid;
+const mapStateToProps = ({ NewsListReducer }) => {
+    const { NewsList } = NewsListReducer;
+    return { NewsList }
+}
+
+const mapDispatchToProps = {
+    getAllNews
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewsGrid);
