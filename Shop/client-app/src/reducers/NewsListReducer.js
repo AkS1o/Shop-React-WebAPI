0df6dd060ca575180b7b2079c80de88b226ec645
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-26 15:37:53 +0300

    Add NewsReducer and change RoodReducer

---
+++ Shop/client-app/src/reducers/NewsListReducer.js
@@ -0,0 +1,38 @@
+﻿const initialState = {
+    NewsList: [],
+    CurrentNews: null
+}
+
+const NewsListReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "NEWS_LOADED":
+            return {
+                ...state,
+                NewsList: action.payload
+            }
+        case "NEWS_ADD":
+            return {
+                ...state,
+                NewsList: action.payload
+            }
+        case "NEWS_DELETE":
+            return {
+                ...state,
+                NewsList: action.payload
+            }
+        case "NEWS_GET":
+            return {
+                ...state,
+                CurrentNews: action.payload
+            }
+        case "NEWS_UPDATE":
+            return {
+                ...state,
+                NewsList: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default NewsListReducer;
--- Shop/client-app/src/reducers/RootReducer.js
+++ Shop/client-app/src/reducers/RootReducer.js
@@ -1,10 +1,12 @@
 import GameListReducer from "./GameListReducer";
 import GenreListReducer from "./GenreListReducer";
 import PlatformListReducer from "./PlatformListReducer"
+import NewsListReducer from "./NewsListReducer"
 import { combineReducers } from "redux";
 
 export default combineReducers({
     GameListReducer,
     GenreListReducer,
-    PlatformListReducer
+    PlatformListReducer,
+    NewsListReducer
 })
