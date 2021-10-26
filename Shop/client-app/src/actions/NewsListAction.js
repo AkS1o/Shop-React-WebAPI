1311dd5a46d1f94b8f7f2412f45e6e45af8b8946
Влад Жомирук <zhomyruk123@gmail.com>
2021-10-26 15:36:08 +0300

    Add NewsActionList

---
+++ Shop/client-app/src/actions/NewsListAction.js
@@ -0,0 +1,34 @@
+﻿export const getAllNews = (newsList) => {
+    return {
+        type: "NEWS_LOADED",
+        payload: newsList
+    }
+}
+
+export const addNews = (newsList) => {
+    return {
+        type: "NEWS_ADD",
+        payload: newsList
+    }
+}
+
+export const deleteNews = (newsList) => {
+    return {
+        type: "NEWS_DELETE",
+        payload: newsList
+    }
+}
+
+export const getCurrentNews = (news) => {
+    return {
+        type: "NEWS_GET",
+        payload: news
+    }
+}
+
+export const changeNews = (newsList) => {
+    return {
+        type: "NEWS_UPDATE",
+        payload: newsList
+    }
+}
