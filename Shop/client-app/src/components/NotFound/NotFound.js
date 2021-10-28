dcf14c30e2f306ff59a5106fa76ce70f62856a27
Віталік <vitalik.baziuk228@gmail.com>
2021-10-28 15:31:58 +0300

    Add NotFound

--- Shop/client-app/src/components/NotFound/NotFound.js
+++ Shop/client-app/src/components/NotFound/NotFound.js
@@ -1,52 +1,96 @@
 import React, { Component } from 'react';
 import { Fragment } from "react";
+import { Link } from 'react-router-dom';
 
 export default class NotFound extends Component {
-    // componentDidMount() {
-    //     window.addEventListener('resize', this.resizeEvent);
-    // }
 
-    // hide() {
-    //     var notFound = document.getElementById('notFound').value;
-    //     console.log("notFound", notFound);
-    // }
+    sleep(time) {
+        return new Promise((resolve) => setTimeout(resolve, time)
+        )
+    }
+
+    async anivation() {
+        var notFound = document.getElementById("content-text");
+        var subtext = document.getElementById("main-subtext");
+
+        var countItem = notFound.children.length;
+        var itemMidle = (countItem - 1) / 2;
+        var itemActive = itemMidle;
+
+        for (let i = itemActive + 1; i < countItem; i++) {
+            await this.sleep(140).then(() => {
+                notFound.children[i].classList.add("active");
+            })
+        }
+        for (let i = itemActive; i < countItem; i++) {
+            await this.sleep(70).then(() => {
+                notFound.children[i].classList.remove("active");
+            })
+        }
+
+        for (let i = 0; i < itemMidle + 1; i++) {
+            await this.sleep(140).then(() => {
+                notFound.children[i].classList.add("active");
+            })
+        }
+        for (let i = 0; i < itemMidle; i++) {
+            await this.sleep(70).then(() => {
+                notFound.children[i].classList.remove("active");
+            })
+        }
+
+        subtext.classList.add("active");
+    }
+
+    componentDidMount() {
+        this.anivation();
+    }
 
     render() {
         return (
             <Fragment>
-                <div className="notFound" id="notFound" onLoad={this.hide}>
-                    <div className="text-wrap" >
-                        <h1 className="top stroke">404</h1>
+                <div className="notFound" id="notFound">
+                    <div className="img-wrap">
+                        <img src="img/salty-justice-NOMebOREvtc-unsplash.jpg" className="img-fluid" alt="img" />
                     </div>
-                    <div className="text-wrap full">
-                        <h1 className="">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="bottom stroke">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="bottom stroke">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="top">404</h1>
-                    </div>
-                    <div className="text-wrap full">
-                        <h1 className="">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="bottom">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="top">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="top stroke">404</h1>
-                    </div>
-                    <div className="text-wrap full">
-                        <h1 className="stroke">404</h1>
-                    </div>
-                    <div className="text-wrap">
-                        <h1 className="bottom">404</h1>
+                    <div className="content-text-wrap" id="content-text">
+                        <span className="text-wrap" >
+                            <span className="text stroke">404</span>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text">404</span>
+                        </span>
+                        <span className="text-wrap full">
+                            <span className="text stroke">404</span>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text stroke">404</span>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text">404</span>
+                        </span>
+                        <span className="text-wrap full active main-text">
+                            <span className="text stroke">404</span>
+                            <div id="main-subtext">
+                                <p>Page not found</p>
+                                <Link to="/home" className="btn btn-primary">Back To Home</Link>
+                            </div>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text bottom">404</span>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text">404</span>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text stroke">404</span>
+                        </span>
+                        <span className="text-wrap full">
+                            <span className="text stroke">404</span>
+                        </span>
+                        <span className="text-wrap">
+                            <span className="text bottom">404</span>
+                        </span>
                     </div>
                 </div>
             </Fragment>
