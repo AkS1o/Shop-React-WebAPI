441775c8e506ac2efd4c9af52d0ffb5ddb3776ee
Віталік <vitalik.baziuk228@gmail.com>
2021-10-28 15:36:09 +0300

    Add Detalis News

---
+++ Shop/client-app/src/components/Details/DetailsNews/DetailsNews.js
@@ -0,0 +1,60 @@
+import { Fragment } from "react";
+
+const DetailsNews = () => {
+    return (
+        <Fragment>
+            <section className="news">
+                <div className="row">
+                    <div className="col-md-8 mx-auto">
+                        <div className="post-header">
+                            <div className="img-wrap">
+                                <img src="../img/news/avel-chuklanov-DUmFLtMeAbQ-unsplash.jpg" alt="img" />
+                            </div>
+                            <div className="content">
+                                <h3>Lorem ipsum dolor sit amet</h3>
+                                <div className="meta">Mar 26, Admin</div>
+                            </div>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Sed augue velit, iaculis nec eleifend vel, facilisis sit
+                                amet sapien. Mauris efficitur urna tellus, ac iaculis
+                                libero elementum ac. Vivamus vehicula scelerisque tellus
+                                sed gravida. Vestibulum quis fermentum elit. Nam pretium
+                                justo eu dictum egestas. Morbi velit neque, commodo eget
+                                fermentum sit amet, pretium id nibh. Pellentesque non
+                                nisl non ex feugiat vestibulum. Integer facilisis nunc
+                                ut venenatis vulputate.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Sed augue velit, iaculis nec eleifend vel, facilisis sit
+                                amet sapien. Mauris efficitur urna tellus, ac iaculis
+                                libero elementum ac. Vivamus vehicula scelerisque tellus
+                                sed gravida. Vestibulum quis fermentum elit. Nam pretium
+                                justo eu dictum egestas. Morbi velit neque, commodo eget
+                                fermentum sit amet, pretium id nibh. Pellentesque non
+                                nisl non ex feugiat vestibulum. Integer facilisis nunc
+                                ut venenatis vulputate.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Sed augue velit, iaculis nec eleifend vel, facilisis sit
+                                amet sapien. Mauris efficitur urna tellus, ac iaculis
+                                libero elementum ac. Vivamus vehicula scelerisque tellus
+                                sed gravida. Vestibulum quis fermentum elit. Nam pretium
+                                justo eu dictum egestas. Morbi velit neque, commodo eget
+                                fermentum sit amet, pretium id nibh. Pellentesque non
+                                nisl non ex feugiat vestibulum. Integer facilisis nunc
+                                ut venenatis vulputate.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Fragment>
+    )
+}
+
+export default DetailsNews;
