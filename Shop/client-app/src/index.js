bbb05a602354e05d7e38711884e724fd504720be
Віталік <vitalik.baziuk228@gmail.com>
2021-10-31 16:39:54 +0200

    Change Style and change js file

--- Shop/client-app/src/index.js
+++ Shop/client-app/src/index.js
@@ -12,13 +12,20 @@ import About from "./views/About/About";
 import Products from "./views/Products/Products";
 import News from "./views/News/News";
 import Contacts from "./views/Contacts/Contacts";
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Admin from "./views/Admin/Admin";
-import NoMatchPage from "./views/NoMatchPage/NoMatchPage";
+import Cart from "./views/Cart/Cart";
 
 import ProductDetails from "./views/Products/ProductDetails"
 import NewsDetails from "./views/News/NewsDetails"
+
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
+import Admin from "./views/Admin/Admin";
+import AdminLogin from "./views/SingIn/AdminSingIn/AdminSingIn";
+
+import User from "./views/User/User";
+
+import NoMatchPage from "./views/NoMatchPage/NoMatchPage";
 
 import "./../node_modules/bootstrap/dist/css/bootstrap-grid.min.css"
 import "./index.scss"
@@ -33,12 +40,18 @@ const App = () => {
                     <Route path="/products" component={() => <Products />} />
                     <Route path="/news" component={News} />
                     <Route path="/contacts" component={Contacts} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/admin" component={Admin} />
+                    <Route path="/cart" component={Cart} />
 
                     <Route path="/productsId" component={ProductDetails} />
                     <Route path="/newsId" component={NewsDetails} />
+                    
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+
+                    <Route path="/admin" component={Admin} />
+                    <Route path="/admin-login" component={AdminLogin} />
+
+                    <Route path="/user" component={User} />
 
                     <Route exact path="*" component={NoMatchPage} />
                 </Switch>
