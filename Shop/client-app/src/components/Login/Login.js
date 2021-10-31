5128e4e086d911d58fbc246a8743ea03d73c9f50
Віталік <vitalik.baziuk228@gmail.com>
2021-10-31 16:33:43 +0200

    Change Login

--- Shop/client-app/src/components/Login/Login.js
+++ Shop/client-app/src/components/Login/Login.js
@@ -5,37 +5,34 @@ const Login = () => {
     return (
         <Fragment>
             <section className="login">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-5">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="">
-                                        <h2>Login</h2>
-                                        <p>Sign in to your account to continue.</p>
-                                    </div>
-                                    <form>
-                                        <div class="form-group">
-                                            <label for="email" class="form-label mt-4">Email address</label>
-                                            <input type="email" class="form-control" id="email" placeholder="Enter email" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="password" class="form-label mt-4">Password</label>
-                                            <input type="password" class="form-control" id="password" placeholder="Password" />
-                                        </div>
-                                        <div class="d-flex justify-content-center">
-                                            <input type="submit" class="btn btn-outline-primary mt-4" value="Sing In" />
-                                        </div>
-                                    </form>
-                                    <div className="">
-                                        <p>
-                                            Not registered?
-                                            <Link> Create account</Link>
-                                        </p>
-                                    </div>
-                                </div>
+                <div className="img-wrap">
+                    <img src="../img/cheng-feng-4pTY4ty7jXM-unsplash.jpg" alt="img" />
+                </div>
+                <div className="card">
+                    <div className="card-header">
+                        <h2 className="title-line">Login</h2>
+                        <p>Sign in to your account to continue.</p>
+                    </div>
+                    <div className="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" placeholder="Enter email" />
                             </div>
-                        </div>
+                            <div class="form-group">
+                                <label for="password" >Password</label>
+                                <input type="password" id="password" placeholder="Password" />
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <input type="submit" class="btn btn-outline-primary mt-4" value="Sing In" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="card-footer">
+                        <p>
+                            Not registered?
+                            <Link> Create account</Link>
+                        </p>
                     </div>
                 </div>
             </section>
