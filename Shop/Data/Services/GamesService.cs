31d8fb6d8083058d0d7287aa6964d6c437af3c36
Василь Колотюк <vasakolotuk02@gmail.com>
2021-10-31 15:50:23 +0200

    add pagination,id,sort

--- Shop/Data/Services/GamesService.cs
+++ Shop/Data/Services/GamesService.cs
@@ -22,8 +22,8 @@ namespace Shop.Data.Services
         }
 
         public void AddGame(GameVM game)
-        {            
-             var _game = new Game()
+        {
+            var _game = new Game()
             {
                 Name = game.Name,
                 Price = game.Price,
@@ -45,8 +45,8 @@ namespace Shop.Data.Services
                 Rec_Storage = game.Rec_Storage,
                 Rec_DirectX = game.Rec_DirectX,
                 Rec_Graphics = game.Rec_Graphics
-             };
-            _context.Games.Add(_game);  
+            };
+            _context.Games.Add(_game);
             _context.SaveChanges();
             foreach (var id in game.GenreIds)
             {
@@ -56,7 +56,7 @@ namespace Shop.Data.Services
                     GenreId = id
                 };
                 _context.GameGenres.Add(_game_genre);
-                _context.SaveChanges();
+
             }
 
             foreach (var id in game.PlatformIds)
@@ -64,10 +64,10 @@ namespace Shop.Data.Services
                 var _game_platform = new GamePlatform()
                 {
                     GameId = _game.Id,
-                PlatformId = id
+                    PlatformId = id
                 };
                 _context.GamePlatforms.Add(_game_platform);
-                _context.SaveChanges();
+
             }
 
         }
@@ -86,14 +86,14 @@ namespace Shop.Data.Services
                 throw new Exception($"Game with id: {id} not found");
             }
         }
-        public Game СhangeGame(GameVM game,int id)
+        public Game СhangeGame(GameVM game)
         {
-            var _game = _context.Games.FirstOrDefault(n => n.Id == id);
+            var _game = _context.Games.FirstOrDefault(n => n.Id == game.Id_Game);
             var _genre = _context.GameGenres;
             var _platform = _context.GamePlatforms;
 
             if (_game != null)
-            { 
+            {
                 _game.Name = game.Name;
                 _game.Price = game.Price;
                 _game.Description = game.Description;
@@ -116,30 +116,30 @@ namespace Shop.Data.Services
                 _game.Rec_DirectX = game.Rec_DirectX;
                 _game.Rec_Graphics = game.Rec_Graphics;
 
-              
-                    foreach (var tx in _genre)
-                    {
-                        if (tx.GameId == id) { _context.GameGenres.Remove(tx); }
-                    }
-                    _context.SaveChanges();
 
-                    foreach (var iD in game.GenreIds)
+                foreach (var tx in _genre)
+                {
+                    if (tx.GameId == game.Id_Game) { _context.GameGenres.Remove(tx); }
+                }
+                _context.SaveChanges();
+
+                foreach (var iD in game.GenreIds)
+                {
+                    var _game_genre = new GameGenre()
                     {
-                        var _game_genre = new GameGenre()
-                        {
-                            GameId = _game.Id,
-                            GenreId = iD
-                        };
-                        _context.GameGenres.Add(_game_genre);
-                    }
-                
+                        GameId = _game.Id,
+                        GenreId = iD
+                    };
+                    _context.GameGenres.Add(_game_genre);
+                }
+
 
                 _context.SaveChanges();
 
 
                 foreach (var tx in _platform)
                 {
-                    if (tx.GameId == id) { _context.GamePlatforms.Remove(tx); }
+                    if (tx.GameId == game.Id_Game) { _context.GamePlatforms.Remove(tx); }
                 }
                 _context.SaveChanges();
 
@@ -154,9 +154,144 @@ namespace Shop.Data.Services
                 }
                 _context.SaveChanges();
             }
-           
+
             return _game;
         }
+
+
+        public GamePaginationVM GetGamesPaginations(int pageNumber)
+        {
+
+            GamePaginationVM gamePaginationVM = new GamePaginationVM();
+
+            IEnumerable<Game> games = _context.Games.ToArray();
+
+
+
+            gamePaginationVM.CountGames = games.Count();
+
+            decimal count = gamePaginationVM.CountGames / 2;
+            gamePaginationVM.PageAll = Convert.ToInt32(Math.Ceiling(count));
+            gamePaginationVM.PageActive = pageNumber + 1;
+
+            if (pageNumber == 0)
+            {
+                gamePaginationVM.Games = games.Take(2).ToList();
+            }
+            else
+            {
+                gamePaginationVM.Games = games.Skip(2 * pageNumber).Take(2).ToList();
+            }
+
+            return gamePaginationVM;
+        }
+
+
+        public GamePaginationVM GetGamesbyGenre(int genreId)
+        {
+
+            GamePaginationVM gamePaginationVM = new GamePaginationVM();
+            IEnumerable<Game> games = _context.Games.ToArray();
+            IEnumerable<GameGenre> gameGenre = _context.GameGenres.ToArray();
+            List<Game> sortedGames = new List<Game>();
+
+            foreach (var item1 in games)
+            {
+                foreach (var item2 in gameGenre)
+                {
+                    if (item2.GenreId == genreId && item1.Id == item2.GameId) { sortedGames.Add(item1); }
+                }
+            }
+
+            gamePaginationVM.Games = sortedGames.ToList();
+
+            return gamePaginationVM;
+        }
+
+
+        public GamePaginationVM GetGamesbyPlatform(int platformId)
+        {
+
+            GamePaginationVM gamePaginationVM = new GamePaginationVM();
+            IEnumerable<Game> games = _context.Games.ToArray();
+            IEnumerable<GamePlatform> gameplatform = _context.GamePlatforms.ToArray();
+            List<Game> sortedGames = new List<Game>();
+
+            foreach (var item1 in games)
+            {
+                foreach (var item2 in gameplatform)
+                {
+                    if (item2.PlatformId == platformId && item1.Id == item2.GameId) { sortedGames.Add(item1); }
+                }
+            }
+
+            gamePaginationVM.Games = sortedGames.ToList();
+
+            return gamePaginationVM;
+        }
+
+        public IEnumerable<GameVM> GetGamesVM()
+        {
+            var games = _context.Games;
+            var gameGenres = _context.GameGenres;
+            var gamePlatform = _context.GamePlatforms;
+            var gamesList = games.ToList();
+            var gameGenresList = gameGenres.ToList();
+            var gamePlatformList = gamePlatform.ToList();
+            List<GameVM> gamesVM = new List<GameVM>();
+            foreach (var item in gamesList)
+            {
+                var gameVM = new GameVM();
+                gameVM.Name = item.Name;
+                gameVM.Price = item.Price;
+                gameVM.Description = item.Description;
+                gameVM.Quantity = item.Quantity;
+                gameVM.Developer = item.Developer;
+                gameVM.Publisher = item.Publisher;
+                gameVM.Image = item.Image;
+                gameVM.ReleaseDate = item.ReleaseDate;
+                gameVM.Min_OS = item.Min_OS;
+                gameVM.Min_Processor = item.Min_Processor;
+                gameVM.Min_Memory = item.Min_Memory;
+                gameVM.Min_Storage = item.Min_Storage;
+                gameVM.Min_DirectX = item.Min_DirectX;
+                gameVM.Min_Graphics = item.Min_Graphics;
+                gameVM.Rec_OS = item.Rec_OS;
+                gameVM.Rec_Processor = item.Rec_Processor;
+                gameVM.Rec_Memory = item.Rec_Memory;
+                gameVM.Rec_Storage = item.Rec_Storage;
+                gameVM.Rec_DirectX = item.Rec_DirectX;
+                gameVM.Rec_Graphics = item.Rec_Graphics;
+
+                List<int> _GenreIds = new List<int>();
+                foreach (var item2 in gameGenresList)
+                {
+                    if (item.Id == item2.GameId)
+                    {
+
+                        _GenreIds.Add(item2.GenreId);
+
+                    }
+                }
+                gameVM.GenreIds = _GenreIds;
+                List<int> _PlatformIds = new List<int>();
+                foreach (var item2 in gamePlatformList)
+                {
+                    if (item.Id == item2.GameId)
+                    {
+
+                        _PlatformIds.Add(item2.PlatformId);
+
+                    }
+                }
+                gameVM.PlatformIds = _PlatformIds;
+                gamesVM.Add(gameVM);
+            }
+            return gamesVM;
+        }
+
+
+
 
     }
 }
