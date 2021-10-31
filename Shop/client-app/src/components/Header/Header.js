d4cc2f7f78ca49e718c11f06d67c2febcb601fa8
Віталік <vitalik.baziuk228@gmail.com>
2021-10-31 16:33:58 +0200

    Change Header

--- Shop/client-app/src/components/Header/Header.js
+++ Shop/client-app/src/components/Header/Header.js
@@ -2,22 +2,29 @@ import React from "react"
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 
+import { faUser, faCog, faUserCircle, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Logo from "../Logo/Logo";
 
 const Header = () => {
-    window.addEventListener('click', (event)=>{
+    const dropdownOpen = () => {
+        let dropdownMenu = document.getElementById('dropdown-menu');
+        dropdownMenu.style.display = 'block';
+
+        window.addEventListener('click', dropdownClose)
+    }
+
+    const dropdownClose = (event) => {
         let dropdown = document.getElementById('dropdown');
         let dropdownToggle = document.getElementById('dropdown-toggle');
 
         if (!dropdown.contains(event.target) && !dropdownToggle.contains(event.target)) {
             let dropdownMenu = document.getElementById('dropdown-menu');
             dropdownMenu.style.display = 'none';
+
+            window.removeEventListener('click', dropdownClose);
         }
-    })
-    
-    const dropdown = () => {
-        let dropdownMenu = document.getElementById('dropdown-menu');
-	    dropdownMenu.style.display = 'block';
     }
 
     return (
@@ -35,17 +42,33 @@ const Header = () => {
                                 <li><Link to="/contacts">Contacts</Link></li>
                             </ul>
                         </nav>
-                        <div id="dropdown" className="dropdown" onClick={dropdown}>
+                        <div className="btn-group">
+                            <Link to="/login" className="btn btn-outline-primary">Login</Link>
+                            <Link to="/register" className="btn btn-primary">Register</Link>
+                        </div>
+                        <div id="dropdown" className="dropdown" onClick={dropdownOpen}>
                             <div id="dropdown-toggle" className="dropdown-toggle">
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                <FontAwesomeIcon icon={faUserCircle} />
                             </div>
-                            <ul id="dropdown-menu">
-                                <li><Link to="#">home</Link></li>
-                                <li><Link to="#">about</Link></li>
-                                <li><Link to="#">galery</Link></li>
-                                <li><Link to="#">setings</Link></li>
+                            <ul id="dropdown-menu" className="dropdown-menu">
+                                <li>
+                                    <Link to="/user/profile">
+                                        <FontAwesomeIcon icon={faUser} />
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/user/settings">
+                                        <FontAwesomeIcon icon={faCog} />
+                                        Setings
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#">
+                                        <FontAwesomeIcon icon={faSignOutAlt} />
+                                        logout
+                                    </Link>
+                                </li>
                             </ul>
                         </div>
                     </div>
