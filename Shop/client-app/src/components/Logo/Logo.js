a5f0b5cbaec00f430b4b45927fd4ba962f152b5f
Віталік <vitalik.baziuk228@gmail.com>
2021-10-31 16:34:29 +0200

    Change Logo

--- Shop/client-app/src/components/Logo/Logo.js
+++ Shop/client-app/src/components/Logo/Logo.js
@@ -3,47 +3,44 @@ import { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
-    // window.addEventListener('load', () => {
-    //     const logo = document.getElementById('logo');
+    const moveLogo = () => {
+        const logo = document.getElementById('logo');
 
-    //     for (var i = 1; i < 7; i++) {
-    //         let div = document.createElement('div');
-    //         div.innerHTML = "logo";
+        if (window.scrollY <= 500) {
+            for (let i = 0; i < logo.children.length; i++) {
+                let top = 100 - (window.scrollY / 5);
 
-    //         if (i < 4)
-    //             div.style.top = `${100 * i}%`;
-    //         else
-    //             div.style.top = `${100 * -i + 300}%`;
+                if (i < 4)
+                    top *= i;
+                else
+                    top *= (-i + 3);
 
-    //         logo.appendChild(div);
-    //     }
-    // });
+                logo.children[i].style.top = `${top}%`;
+            }
+        }
+        else if (window.scrollY > 500) {
+            for (let i = 0; i < logo.children.length; i++)
+                logo.children[i].style.top = `${0}%`;
+        }
+    }
 
-    // window.addEventListener('scroll', () => {
-    //     const logo = document.getElementById('logo');
-
-    //     if (window.scrollY <= 500) {
-    //         for (let i = 0; i < logo.children.length; i++) {
-    //             let top = 100 - (window.scrollY / 5);
-
-    //             if (i < 4)
-    //                 top *= i;
-    //             else
-    //                 top *= (-i + 3);
-
-    //             logo.children[i].style.top = `${top}%`;
-    //         }
-    //     }
-    //     else if (window.scrollY > 500) {
-    //         for (let i = 0; i < logo.children.length; i++)
-    //             logo.children[i].style.top = `${0}%`;
-    //     }
-    // });
+    React.useEffect(() => {
+        window.addEventListener('scroll', moveLogo);
+        return () => {
+            window.removeEventListener('scroll', moveLogo);
+        };
+    });
 
     return (
         <Fragment>
             <div className="logo" id="logo">
                 <Link to="#">logo</Link>
+                <div>logo</div>
+                <div>logo</div>
+                <div>logo</div>
+                <div>logo</div>
+                <div>logo</div>
+                <div>logo</div>
             </div>
         </Fragment>
     )
