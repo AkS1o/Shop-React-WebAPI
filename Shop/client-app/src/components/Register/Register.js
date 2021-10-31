5c933393f2fc07e8a4c068baff9871afc15054ed
Віталік <vitalik.baziuk228@gmail.com>
2021-10-31 16:34:15 +0200

    Change Register

--- Shop/client-app/src/components/Register/Register.js
+++ Shop/client-app/src/components/Register/Register.js
@@ -4,35 +4,32 @@ const Register = () => {
     return (
         <Fragment>
             <section className="register">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-5">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="">
-                                        <h2>Register</h2>
-                                        <p>Create your account.</p>
-                                    </div>
-                                    <form>
-                                        <div class="form-group">
-                                            <label for="fullname" class="form-label mt-4">Full Name</label>
-                                            <input type="text" class="form-control" id="fullname" placeholder="Enter your name" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="email" class="form-label mt-4">Email address</label>
-                                            <input type="email" class="form-control" id="email" placeholder="Enter your email" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="password" class="form-label mt-4">Password</label>
-                                            <input type="password" class="form-control" id="password" placeholder="Enter your Password" />
-                                        </div>
-                                        <div class="d-flex justify-content-center">
-                                            <input type="submit" class="btn btn-outline-primary mt-4" value="register" />
-                                        </div>
-                                    </form>
-                                </div>
+                <div className="img-wrap">
+                    <img src="../img/cheng-feng-4pTY4ty7jXM-unsplash.jpg" alt="img" />
+                </div>
+                <div className="card">
+                    <div className="card-header">
+                        <h2>Register</h2>
+                        <p>Create your account.</p>
+                    </div>
+                    <div className="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="fullname">Full Name</label>
+                                <input type="text" id="fullname" placeholder="Enter your name" />
                             </div>
-                        </div>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" placeholder="Enter your email" />
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" placeholder="Enter your Password" />
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <input type="submit" class="btn btn-outline-primary mt-3" value="register" />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </section>
