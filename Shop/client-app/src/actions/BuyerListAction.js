8477ba58cf890bc618b5302634622f0f4e5273f3
Влад Жомирук <zhomyruk123@gmail.com>
2021-11-05 11:46:54 +0200

    Add BuyerListAction and BuyerListReducer

---
+++ Shop/client-app/src/actions/BuyerListAction.js
@@ -0,0 +1,34 @@
+﻿export const getAllBuyers = (buyersList) => {
+    return {
+        type: "BUYERS_LOADED",
+        payload: buyersList
+    }
+}
+
+export const addBuyers = (buyersList) => {
+    return {
+        type: "BUYER_ADD",
+        payload: buyersList
+    }
+}
+
+export const deleteBuyer = (buyersList) => {
+    return {
+        type: "BUYER_DELETE",
+        payload: buyersList
+    }
+}
+
+export const getCurrentBuyer = (buyer) => {
+    return {
+        type: "BUYER_GET",
+        payload: buyer
+    }
+}
+
+export const changeBuyer = (buyersList) => {
+    return {
+        type: "BUYER_UPDATE",
+        payload: buyersList
+    }
+}
---
+++ Shop/client-app/src/reducers/BuyerListReducer.js
@@ -0,0 +1,38 @@
+﻿const initialState = {
+    BuyersList: [],
+    CurrentBuyer: null
+}
+
+const BuyerListReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "BUYERS_LOADED":
+            return {
+                ...state,
+                BuyersList: action.payload
+            }
+        case "BUYER_ADD":
+            return {
+                ...state,
+                BuyersList: action.payload
+            }
+        case "BUYER_DELETE":
+            return {
+                ...state,
+                BuyersList: action.payload
+            }
+        case "BUYER_GET":
+            return {
+                ...state,
+                CurrentBuyer: action.payload
+            }
+        case "BUYER_UPDATE":
+            return {
+                ...state,
+                BuyersList: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default BuyerListReducer;
